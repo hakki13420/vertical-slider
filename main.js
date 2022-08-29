86b9ba27093604df4374aa743d6c5c606ddd886f
hakki <hak_rab@yahoio.fr>
2022-08-29 12:36:38 +0100

    vertical slider

---
+++ main.js
@@ -0,0 +1,33 @@
+const slideRightWrapper=document.querySelector('.sliderRightWrapper')
+const slideWrapper=document.querySelector('.slideWrapper')
+
+const slideRights=document.querySelectorAll('.slideRight')
+const slides=document.querySelectorAll('.slide')
+let selectedSlide=0
+
+function initialiseSlideRight(){
+    slideRights.forEach((slide)=>{
+        slide.classList.remove('selected')
+    })
+}
+
+function movingRightSloder(){
+    setTimeout(() => {
+        slideRightWrapper.style.transform=`translateY(${-120*selectedSlide}px)`
+        slideWrapper.style.transform=`translateX(${-100*selectedSlide}vw)`
+        slideRightWrapper.style.overflowY='hidden'
+        initialiseSlideRight()
+        slideRights[selectedSlide].classList.add('selected')        
+        selectedSlide++;
+
+        if(selectedSlide==5) {
+            slideWrapper.style.transform=`translateX(${100*(0)}vw)`
+            selectedSlide=0            
+        }
+
+        movingRightSloder()
+    }, 4000);
+    
+}
+
+movingRightSloder()
